2ad7fc9e3e3789bdfeb828c765edfece56ab659b
vik <me@vkrm.ch>
2019-03-18 18:31:30 -0400

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
+
+if [[ -e /config ]]
+    then
+        cp -f /audcon/audcon/cfg/default.json /config/default.json
+        rm -rf /audcon/audcon/cfg
+        ln -s /config /audcon/audcon/cfg
+fi
+
 cd /audcon
 FLASK_APP=app.py FLASK_DEBUG=1 flask run -h 0.0.0.0
