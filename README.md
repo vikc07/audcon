4b88a27081893b94933dbb476f0fe7c7c572d6e1
vik <me@vkrm.ch>
2019-03-18 18:04:03 -0400

    added database.sql

--- README.md
+++ README.md
@@ -9,11 +9,18 @@ and stores metadata in the database while queuing files with unsupported format 
 scans the queue and converts audio files.
 
 ### Requirements ###
-* MySQL database is required to be pre-created. Use utf8mb4 charset
-* ffprobe and ffmpeg are used for scanning and converting respectively. Ensure these programs are available locally 
-and in the PATH variable
+1. MySQL Server v8.x
+2. `ffmpeg` and `ffprobe` installed locally with commands available in the `PATH` variable
 
-A Docker image is available with Audcon and ffmpeg/ffprobe pre-installed. You will still need mysql container
+### Installation ###
+1. Clone the git repository to local machine
+2. Create MySQL Database and tables using `database.sql` file prior to running. Edit the user/pass in the script as 
+needed
+3. Execute `run.sh` to start the internal server
+4. Access the web ui at `http://server:5000`. First time you run it will ask for changing settings
+
+Alternatively, a Docker image is available with Audcon and ffmpeg/ffprobe pre-installed. You will still need MySQL 
+container.
 
 #### ---- Experimental Only ---- ####
 Understand this is a very raw product. Therefore, use at own risk
---
+++ database.sql
@@ -0,0 +1,61 @@
+CREATE DATABASE audcon_dev CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE USER 'audcon_dev'@'%' IDENTIFIED BY 'audcon_dev_123';
+GRANT ALL PRIVILEGES ON audcon_dev.* To 'audcon_dev'@'%';
+
+CREATE TABLE `media` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_date` datetime NOT NULL,
+  `modified_date` datetime NOT NULL,
+  `isdeleted` int(11) DEFAULT NULL,
+  `media_file_path` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `media_title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `media_fsize` bigint(20) NOT NULL,
+  `media_format` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `media_streams_count` smallint(6) NOT NULL,
+  `media_a_streams_count` smallint(6) NOT NULL,
+  `media_v_streams_count` smallint(6) NOT NULL,
+  `media_s_streams_count` smallint(6) NOT NULL,
+  `media_o_streams_count` smallint(6) NOT NULL,
+  `media_a_codec` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `media_a_sample_fmt` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `media_a_sample_rate` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `media_a_channels` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `media_a_channel_layout` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `media_a_bitrate` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `media_full_meta` json DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `queue` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_date` datetime NOT NULL,
+  `modified_date` datetime NOT NULL,
+  `isdeleted` int(11) DEFAULT NULL,
+  `media_file_path` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `media_output_file_path` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `media_output_ffmpeg_params` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `media_converted` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `run_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_date` datetime NOT NULL,
+  `modified_date` datetime NOT NULL,
+  `isdeleted` int(11) DEFAULT NULL,
+  `service` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `params` json DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE `run_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created_date` datetime NOT NULL,
+  `modified_date` datetime NOT NULL,
+  `isdeleted` int(11) DEFAULT NULL,
+  `service` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `params` json DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
